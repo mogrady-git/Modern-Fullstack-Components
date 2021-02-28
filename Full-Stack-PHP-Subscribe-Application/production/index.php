a6f5b5ee406516a9b44991fa2a1002e3070238a9
Michael O'Grady <michaelogradyofficial@gmail.com>
2021-02-28 01:04:47 +0000

    addition of post to database script and production version

--- Full-Stack-PHP-Subscribe-Application/production/index.php
+++ Full-Stack-PHP-Subscribe-Application/production/index.php
@@ -8,6 +8,44 @@
     <title>Subscription App | Full Stack PHP Subscriber Application</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
+    <?php
+    $conn = mysqli_connect("localhost", "root", "root", '');
+    if (!$conn)
+        die("Connection error" . mysqli_error($conn));
+    echo "<p style='color:white; background:green'>Connection successful...</p>";
+    // Check if users Database Exists
+    $db = mysqli_select_db($conn, "users");
+    if (empty($db)) {
+        echo "<p style='color:white; background:red'>Database was not found!</p>";
+
+        // Create users Database
+        $dbcr = "create database users";
+        $check = mysqli_query($conn, $dbcr);
+        if (!$check)
+            echo "Database Create Error";
+        else echo "<p style='color:white; background:green; font-weight:bold'>Database was created successfully!</p>";
+    } else {
+        echo "<p style='color:white; background:green; font-weight:bold'>Database already exists...!</p>";
+        // Creates table user_info if not exist
+        $table = "SELECT * FROM user_info";
+        $checktable = mysqli_query($conn, $table);
+        if (!$checktable) {
+            echo "<p style='color:white; background:red'>The Table was not found, please create the Table...!</p>";
+            // Creating the Table
+            $createTBL = "CREATE TABLE user_info (
+            id int(100) NOT NULL AUTO_INCREMENT,
+            name varchar(255) NOT NULL,
+            email varchar(255) NOT NULL,
+            password varchar(255) NOT NULL,
+            PRIMARY KEY(id)
+            )";
+            $ok = mysqli_query($conn, $createTBL);
+        } else {
+            echo "<p style='color:white; background:green; font-weight:bold'>Table already exists...!</p>";
+        }
+    };
+
+    ?>
 </head>
 
 <body>
@@ -36,10 +74,19 @@
             ?>
                         <!-- Styling for Success Message if Email is Sent Correctly! -->
                         <div class="alert success-alert"><?php echo $userEmail ?><br>Thanks for Subscribing!</div>
-                    <?php
+                        <?php
+
+                        $sql = "INSERT INTO user_info (name, email, password)
+                        VALUES ('','$userEmail','')";
+
+                        if (mysqli_query($conn, $sql)) {
+                            echo "New record created successfully";
+                        } else {
+                            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                        };
                         $userEmail = ""; // Again, leave email field blank once email is sent
                     } else {
-                    ?>
+                        ?>
                         <!-- Styling an error message if the email can't be sent -->
                         <div class="alert error-alert">Failed while sending your email!</div>
                     <?php
@@ -62,7 +109,6 @@
             </div>
         </form>
         <div class="text">We do not share your information.</div>
-        <p><a href="production/index.php">Move to the Production Version</a></p>
     </div>
 </body>
 
